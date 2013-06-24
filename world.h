8bc2ae631e7fbe0e8ad437103d44caf6ddcd410e
dcmoyer <dcmoyer@gmail.com>
2013-06-24 14:20:14 -0700

    World Class Header

---
+++ world.h
@@ -0,0 +1,28 @@
+/*
+
+This is the World class header for the Contagion Project, UCLA REU 2013.
+
+062413 1150 Created. (DM)
+
+
+*/
+
+
+#ifndef WORLD_H
+#define WORLD_H
+
+#include "Agent.h"
+
+class world {
+public:
+    
+    
+    
+private:
+
+};
+
+
+#endif
+
+
