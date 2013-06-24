42c6682911a770615ad8f5f8a83f07993d4f75bd
dcmoyer <dcmoyer@gmail.com>
2013-06-24 14:55:32 -0700

    Re-sync

--- world.h
+++ world.h
@@ -1,28 +1,28 @@
 /*
-
-This is the World class header for the Contagion Project, UCLA REU 2013.
-
-062413 1150 Created. (DM)
-
-
-*/
+ 
+ This is the World class header for the Contagion Project, UCLA REU 2013.
+ 
+ 062413 1150 Created. (DM)
+ 
+ 
+ */
 
 
 #ifndef WORLD_H
 #define WORLD_H
 
-#include "Agent.h"
+#include "constants.h"
+#include "agent.h"
 
 class world {
 public:
-    
-    
+    agent* a[];
+    cells
     
 private:
-
+    
 };
 
 
 #endif
-
 
