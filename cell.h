1814fbe112cc53c44eb5abfa6870c5d4fbb1deec
linggeli7 <linggeli7@gmail.com>
2013-06-25 15:30:07 -0600

    THE COPY CONSTRUCTOR, ASSIGNMENT OPERATOR AND DESTRUCTOR ARE MISSING!

--- cell.h
+++ cell.h
@@ -5,6 +5,8 @@
  062413 1457 Created (DM)
  062413 1649 Added constructors, accessors, and mutators for cell_node, and a
     default constructor for cell. (DM)
+
+ THE COPY CONSTRUCTOR, ASSIGNMENT OPERATOR AND DESTRUCTOR ARE MISSING!
  
 */
 #ifndef CELL_H
@@ -15,51 +17,104 @@
 
 /*
  CELL:
- Main Class. It's so classy.
+ Main Class. It's so classy. It's implemented as a singly-linked list of agent pointers.
  
 */
 class cell{
 //private:
 
 public:
-    cell_node* top; //WILL BE LINKED LIST LATER
-    std::vector<cell*>;
+    cell_node* top; 
+	cell_node* current; 
+
+    std::vector<cell*> neighbors; 
     
     double center_x;
-    double center_y;
+    double center_y; 
     
+	/*
+	Default constructor
+	*/
     cell();
-    cell(vector<cell*> &neighbors, vector<agents*> &agents);
+
+	/*
+	Fancy constructor
+	*/
+    cell(vector<agents*> &agents);
     
-    void set_neighbors(vector<cell*> );
+	/*
+	Set neighbors
+	*/
+    void set_neighbors(vector<cell*> &neighbors); 
     
+	/*
+	Add to the beginning of the list
+	*/
+	void add_top(cell_node new_node);
+
+	/*
+	Remove the first node in the list
+	*/
+	void remove_top();
+
+	/*
+	Get the first node in the list
+	*/
+	cell_node get_top();
+
+	/*
+	Move the current pointer to the next node
+	*/
+	void next();
+
+	/*
+	Reset current pointer to the top
+	*/
+	void reset_current();
+
     friend class cell_node; //Yes, bad practice, but they're tied. Don't do this at home kids.
 };
 
 
 /*
  CELL_NODE:
- This is a two way linked list. It is intended to allow a cell to pass agents easily.
- 
  AFTER UPDATE, for epsilon remove a node from list or remove it as a possibility from being checked by anyone.
  
  */
 class cell_node{
 private:
-    cell_node* toward_top;
-    cell_node* away_from_top;
+    cell_node* next;
     agent* target_agent;
     
+	/*
+	Default constructor
+	*/
     cell_node();
+
+	/*
+	Fancy constructor
+	*/
     cell_node(agent* target);
+
+	/*
+	Set the next node
+	*/
+    void set_next(cell_node* &next_node);
+
+	/*
+	Set the agent 
+	*/
+    void set_agent(agent* &target);
     
-    void set_toward_top(cell_node* &up);
-    void set_away_from_top(cell_node* &down);
-    void set_target_agent(agent* &target);
-    
-    cell_node* get_toward_top();
-    cell_node* get_away_from_top();
-    agent* get_target_agent();
+	/*
+	Return the next node
+	*/
+    cell_node* get_next();
+
+	/*
+	Return the agent
+	*/
+    agent* get_agent();
     
     friend class cell; //Yes, bad practice, but they're tied. Don't do this at home kids.
     
