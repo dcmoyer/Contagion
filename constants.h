17aaec6c33d2731320da0ede3f1b667102d7d4ba
dougyd92 <douglasdejesus92@gmail.com>
2013-06-26 10:02:05 -0700

    Update constants.h
    added step_size constant

--- constants.h
+++ constants.h
@@ -14,8 +14,8 @@ This is a header for global constants for the Contagion Project, UCLA REU 2013.
 #define NUM_OF_AGENTS   10
 #define DOMAIN_DIM_1    10
 #define DOMAIN_DIM_2    10
-#define CELL_LENGTH
-
+#define CELL_LENGTH   60
+#define STEP_SIZE .1
 #define HIST_LENGTH 1
 
 #endif
