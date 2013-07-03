a4e4e33accfa82f78bb69eb45bff316ad7ccf90d
Doug de Jesus <douglasdejesus92@gmail.com>
2013-07-03 11:56:33 -0700

    calibrated some constants

--- main.cpp
+++ main.cpp
@@ -1,22 +1,24 @@
 #include "world.h"
 #include <fstream>
+#include <ctime>
 
 int main()
 {
 	std::ofstream str;
-	str.open("data.txt");
+	time_t t = time(NULL);
+	str.open("C:\\Users\\dougyd\\Desktop\\data1.txt");
 	world w = world();
-	w.populate_rand(2500);
-	for(int i = 1; i <=2500
-		; i++)
+	w.populate_rand(100);
+	for(int i = 1; i <=5000; i++)
 	{
 		w.update_forward_velocs();
 		w.update_agent_pos();
 		w.move_to_cell();
-		if(i % 10 == 0){
-			std::cout << i << "\n";
+		if(i % 25 == 0){
+			std::cout << i << " " << time(NULL) - t << " total seconds elapsed" << "\n";
 			w.print(str);
 		}
+		//w.print(str);
 		//w.print(std::cout);
 	}
 	str.close();
