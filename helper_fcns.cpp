d71f905b274990b774087a9b4dbe506d384ccab8
dougyd92 <douglasdejesus92@gmail.com>
2013-07-09 18:02:45 -0700

    added emotion model, epsilon cutoff

--- helper_fcns.cpp
+++ helper_fcns.cpp
@@ -89,7 +89,14 @@ void swarm1(agent* me, agent* you)
 	double x2 = you->get_x_coord();
 	double y2 = you->get_y_coord();
 	//double z2 = (*you).get_z_coord();
+	//calculate distances
+	double dx= x2-x1;
+	double dy= y2-y1;
+	//double dz= z2-z1;
+	double r = sqrt(dx*dx + dy*dy /*+ dz*dz*/);
 
+	if(r<CELL_LENGTH)
+	{
 	//get component-wise velocity
 	double vx1 = me->get_x_veloc_index(0);
 	double vy1 = me->get_y_veloc_index(0);
@@ -97,16 +104,10 @@ void swarm1(agent* me, agent* you)
 	double vx2 = you->get_x_veloc_index(0);
 	double vy2 = you->get_y_veloc_index(0);
 	//double vz2 = (*you).get_z_veloc_index(0);
-
-	//calculate distances
-	double dx= x2-x1;
-	double dy= y2-y1;
-	//double dz= z2-z1;
 	double dvx = vx2-vx1;
 	double dvy = vy2-vy1;
 	//double dvz = vz2-vz1;
-	double r = sqrt(dx*dx + dy*dy /*+ dz*dz*/);
-	//double sum = std::abs(dx)+std::abs(dy)/*+std::abs(dz)*/;
+	
 
 	//calculate attraction/repulsion forces
 	//double u = -C_A * exp(-r / L_A) + C_R * exp(-r / L_R);
@@ -124,6 +125,8 @@ void swarm1(agent* me, agent* you)
     
     me->add_to_x_accel(fx);
     me->add_to_y_accel(fy);
+	//you->add_to_x_accel(-fx);
+   // you->add_to_y_accel(-fy);
 	/*(*me).set_forward_v_x((*me).get_forward_v_x()+fx);
 	(*me).set_forward_v_y((*me).get_forward_v_y()+fy);
 	(*me).set_forward_v_z((*me).get_forward_v_z()+fz);*/
@@ -132,7 +135,7 @@ void swarm1(agent* me, agent* you)
 	//(*you).set_forward_v_z((*you).get_forward_v_z()+fz);
     
     me->iterate_NearestNeighbor();
-
+	}
 
 }
 void swarm_attract(agent* me, agent* you)
@@ -172,6 +175,82 @@ void swarm_attract(agent* me, agent* you)
     me->iterate_NearestNeighbor();
 
 }
+
+
+void swarm1_fear(agent* me, agent* you)
+{
+	//get x,y,z coords
+	double x1 = me->get_x_coord();
+	double y1 = me->get_y_coord();
+	//double z1 = (*me).get_z_coord();
+	double x2 = you->get_x_coord();
+	double y2 = you->get_y_coord();
+	//double z2 = (*you).get_z_coord();
+	//calculate distances
+	double dx= x2-x1;
+	double dy= y2-y1;
+	//double dz= z2-z1;
+	double r = sqrt(dx*dx + dy*dy /*+ dz*dz*/);
+
+	if(r<CELL_LENGTH)
+	{
+	//get component-wise velocity
+	double vx1 = me->get_x_veloc_index(0);
+	double vy1 = me->get_y_veloc_index(0);
+	//double vz1 = (*me).get_z_veloc_index(0);
+	double vx2 = you->get_x_veloc_index(0);
+	double vy2 = you->get_y_veloc_index(0);
+	//double vz2 = (*you).get_z_veloc_index(0);
+	double dvx = vx2-vx1;
+	double dvy = vy2-vy1;
+	//double dvz = vz2-vz1;
+	
+	//get fear
+	double q1 = me->get_q_mag();
+	double q2 = you->get_q_mag();
+	double dq = q2-q1;
+
+	//calculate attraction/repulsion forces
+	//double u = -C_A * exp(-r / L_A) + C_R * exp(-r / L_R);
+	double ugrad = C_A/L_A * exp(-r / L_A) - C_R/L_R * exp(-r / L_R);
+
+	//calculate alignment forces
+	double h = KAPPA / pow((SIGMA*SIGMA + r*r), GAMMA);
+
+
+	//update velocities
+	double fx = ugrad*dx/r;// - h*dvx;
+	double fy = ugrad*dy/r;// - h*dvy;
+
+
+	//double fx = -h*dvx;
+	//double fy = -h*dvy;
+	//double fz = u*dz/sum + h*dvz;
+    
+    me->add_to_x_accel(fx);
+    me->add_to_y_accel(fy);
+
+	//update fear
+	double fq = me->get_q_change_index(0) + h*dq;
+	me->set_q_change_0(fq);
+
+
+
+	//you->add_to_x_accel(-fx);
+   // you->add_to_y_accel(-fy);
+	/*(*me).set_forward_v_x((*me).get_forward_v_x()+fx);
+	(*me).set_forward_v_y((*me).get_forward_v_y()+fy);
+	(*me).set_forward_v_z((*me).get_forward_v_z()+fz);*/
+	//(*you).set_forward_v_x((*you).get_forward_v_x()+fx);
+	//(*you).set_forward_v_y((*you).get_forward_v_y()+fy);
+	//(*you).set_forward_v_z((*you).get_forward_v_z()+fz);
+    
+    me->iterate_NearestNeighbor();
+	}
+
+}
+
+
 void go_left_test(agent* me, agent* you){
 
     me->add_to_x_accel( (- 1.0 /*+ me->get_x_veloc_index(0)*/) / (double) NUM_OF_AGENTS);
